24d3e7df2fa28122d4bff6527f8e0894c26448d3
fantomphreaker <navaneethgopal4991@gmail.com>
2021-02-11 10:04:13 +0530

    Made changes in sign in component

--- gothamcaresapp/src/app/signin/signin.component.ts
+++ gothamcaresapp/src/app/signin/signin.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SigninserviceService } from '../signinservice.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -10,21 +11,40 @@ import { SigninserviceService } from '../signinservice.service';
 })
 export class SigninComponent implements OnInit {
   public response;
+ 
 
-  constructor(private _router:Router, private signinservice:SigninserviceService) { }
+  constructor(private fb: FormBuilder, private _router:Router, private signinservice:SigninserviceService) { }
+
+  signinform = this.fb.group({
+    email: [''],
+    password: [''],
+  });
+
   
-  onSubmit(data){
-    this.navigateToAdmin();
-  //   this.response = this.signinservice.getData(data).subscribe(x => {if(x == "Login Successful"){
-  //     this.response = true;
-  //   }else{this.response = false;}
-  // });
-  //   // if(this.response == "Login Success"){
-  //   //   this.navigateToAdmin();
-  //   // }
-  //   console.log(this.response);
+  
+  onSubmit(){
+    // this.navigateToAdmin();
+    this.signinservice.login(this.signinform.controls["email"].value, this.signinform.controls["password"].value).subscribe(response =>{
+      this.response = response;
+      console.log(this.response);
+      // this.checkResponse();
+      if(this.response.response == "Login Success"){
+        this.navigateToAdmin();
+      }
+    });
+    // if(this.response == "Login Success"){
+    //   this.navigateToAdmin();
+    // }
+    //console.log(this.response);
+  }
+  
+  private checkResponse(): void{
+    // if(this.response.message == "Login Success"){
+    //   this.navigateToAdmin();
+    // }
+    //this.navigateToAdmin()
     
-}
+  }
 
  
   navigateToAdmin(){
--- gothamcaresapp/src/app/signinservice.service.ts
+++ gothamcaresapp/src/app/signinservice.service.ts
@@ -9,7 +9,8 @@ export class SigninserviceService {
   private apiurl = "https://localhost:44352/api/Admin/Login/";
   constructor(private http: HttpClient) { }
   
-  getData(data){
-    return this.http.get(this.apiurl+data.Email+"/"+data.Password);
+  
+  login(email, password){
+    return this.http.get(this.apiurl+email+"/"+password);
   }
 }
